16a34db4ac20e33452b63e7f534bcd280d72b3d4
Wolffoner <wolffoner@gmail.com>
2022-08-17 17:14:22 -0300

    styles in line

--- src/screens/Login/Login.screen.tsx
+++ src/screens/Login/Login.screen.tsx
@@ -1,18 +1,44 @@
 import { ServerUser, serverUserEmpty } from "@/models";
-import { Container } from "@mui/material";
-import { url } from "inspector";
+import { Box } from "@mui/material";
 import { useState } from "react";
 import { EmailInput, PasswordInput } from "./components";
+
+const styles = {
+    container: {
+        display: 'flex',
+        minWidth: '100%',
+        minHeight: '100%',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        backgroundImage: `url('/background-image.jpg')`,
+        backgroundSize: 'cover'
+    },
+    formContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'absolute',
+        left: 0, top: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        width: { xs: '100%', sm: '80%', md: '60%', lg: '40%', xl: "20%" },
+        height: '100%',
+        gap: '1rem'
+    }
+}
 
 export const LoginView = () => {
 
     const [user, setUser] = useState<ServerUser>(serverUserEmpty);
 
     return (
-        <Container sx={{ display: 'flex', minWidth: '100%', minHeight: '100%', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '1rem', backgroundImage: `url('/background-image.jpg')`, backgroundSize: 'cover' }}>
-            <EmailInput />
-            <PasswordInput />
-            <button>{"sign in"}</button>
-        </Container>
+        <Box sx={styles.container}>
+            <Box sx={styles.formContainer}>
+                <EmailInput />
+                <PasswordInput />
+                <button>{"sign in"}</button>
+            </Box>
+        </Box >
     )
 }
